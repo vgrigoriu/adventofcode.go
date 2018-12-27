4e5a3f741fd179fe6c7748e30c7e516862a7da18
Victor Grigoriu <vgrigoriu@gmail.com>
2018-12-27 20:06:42 +0200

    part 1

--- 2018/day03/main.go
+++ 2018/day03/main.go
@@ -2,16 +2,35 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/vgrigoriu/adventofcode.go/common"
 )
 
+var fabric [1000][1000]int
+
 func main() {
 	lines := common.Input()
 
-	for _, line := range lines {
-		fmt.Println(line)
+	claims := make([]claim, len(lines))
+	for i, line := range lines {
+		claims[i] = new(line)
 	}
+
+	for _, claim := range claims {
+		claim.stake()
+	}
+
+	conflicts := 0
+	for i := 0; i < 1000; i++ {
+		for j := 0; j < 1000; j++ {
+			if fabric[i][j] > 1 {
+				conflicts++
+			}
+		}
+	}
+
+	fmt.Println(conflicts)
 }
 
 type claim struct {
@@ -21,6 +40,21 @@ type claim struct {
 func new(s string) claim {
 	// #123 @ 3,2: 5x4
 	var id, left, top, w, h int
-	fmt.Sscanf(s, "#%d @ %d,%d: %dx%d", &id, &left, &top, &w, &h)
+	_, err := fmt.Sscanf(s, "#%d @ %d,%d: %dx%d", &id, &left, &top, &w, &h)
+	if err != nil {
+		log.Fatalf("cannot parse a claim out of «%s»", s)
+	}
 	return claim{left, top, w, h}
+}
+
+func (c claim) stake() {
+	for i := c.left; i < c.left+c.w; i++ {
+		for j := c.top; j < c.top+c.h; j++ {
+			fabric[i][j]++
+		}
+	}
+}
+
+func (c claim) String() string {
+	return fmt.Sprintf("left: %d; top: %d; w: %d; w: %d", c.left, c.top, c.w, c.h)
 }
