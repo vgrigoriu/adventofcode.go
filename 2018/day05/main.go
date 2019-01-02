37d7cd56c5f371b898f6797ba7aecd673d5c5b78
Victor Grigoriu <vgrigoriu@gmail.com>
2019-01-02 19:31:14 +0200

    part 1

--- 2018/day05/main.go
+++ 2018/day05/main.go
@@ -7,5 +7,20 @@ import (
 
 func main() {
 	input := common.SingleLineInput()
-	fmt.Println(input)
+	result, more := react(input)
+	for more {
+		result, more = react(result)
+	}
+
+	fmt.Println(len(result))
+}
+
+func react(input string) (result string, more bool) {
+	for i := range input[:len(input)-1] {
+		r1, r2 := rune(input[i]), rune(input[i+1])
+		if r1-r2 == 32 || r2-r1 == 32 {
+			return input[:i] + input[i+2:], true
+		}
+	}
+	return input, false
 }
